77998e485cdb32f64d36dce7535d267e6a761ca1
Hadi Abou Hawili <aboualhood2005@hotmail.com>
2025-11-06 00:15:04 +0200

    login/signup page changes.

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.css'
 
 import user_icon from '../../assets/person.png'
@@ -7,23 +7,25 @@ import password_icon from '../../assets/password.png'
 
 export const LoginSignup = () => {
 
-  //TO DO:
-  //const [action, setAction] = useState("Sign Up"); //action is our current state, setAction is the function that will update our state
-  // we use this to update Sign Up to Login when we press the login button
+  const [action, setAction] = useState("Sign Up");
+
   return (
     <div className='container'>
 
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
 
       {/* Name */}
       <div className="inputs">
-        <div className="input">
+        {action === "Login" ? <div></div> :
+          <div className="input">
           <img src={user_icon} alt="user icon" />
           <input type="text" placeholder='Name' />
         </div>
+        }
+        
       </div>
 
       {/* email */}
@@ -42,12 +44,13 @@ export const LoginSignup = () => {
         </div>
       </div>
 
-      <div className="forgot-password"><span>Forgot password?</span></div>
+      {action === "Sign Up" ? <div></div> : <div className="forgot-password"><span>Forgot password?</span></div>}
 
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div className={action === "Login" ? "submit gray" : "submit"} onClick={() =>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() =>{setAction("Login")}}>Login</div>
       </div>
     </div>
+    //TODO: better practice is to use routes to redirect the user to a /login page and /signup page upon button press
   )
 }
